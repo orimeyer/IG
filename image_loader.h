ba0cce05bb6df5873b1f81c69386e8eeb57cc559
orimeyer <ori.meyer@campus.technion.ac.il>
2025-07-27 12:02:43 +0300

    fix

--- image_loader.h
+++ image_loader.h
@@ -2,6 +2,6 @@
 #ifndef IMAGE_LOADER_H
 #define IMAGE_LOADER_H
 
-unsigned char* load_imag(const char* filename, int& width, int& height, int& channels);
+unsigned char* load_image(const char* filename, int& width, int& height, int& channels);
 
 #endif // IMAGE_LOADER_H
